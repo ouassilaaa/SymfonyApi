f1d065273d48e5be86c009d7cb08bb7a5651caad
ouassilaaa <ouassidu3@hotmail.fr>
2023-05-30 13:56:36 +0200

    env

---
+++ src/Service/ApiRegister.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Service;
+use App\Repository\UserRepository;
+use App\Service\Utils;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+    class ApiRegister{
+
+    //fonction pour tester l'authentification
+public function authentification(UserPasswordHasherInterface $hash, UserRepository $repo,
+string $mail, string $password){
+//nettoyer avec la classe Utils et cleanInputStatic
+$mail = Utils::cleanInputStatic($mail);
+$password = Utils::cleanInputStatic($password);
+//récupérer le compte utilisateur
+$user = $repo->findOneBy(['email'=>$mail]);
+//tester si le compte existe
+if($user){
+    //tester le password
+    if($hash->isPasswordValid($user, $password)){
+        return true;
+     }
+     else{
+        return false;
+     }
+ }
+ //test sinon le compte n'existe pas
+ else{
+    return false;
+ }
+}
+    }
+?>
